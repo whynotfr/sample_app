c894cc126e7a1b715004868ccff35d700d5e559f
whynotfr <charles@offwefly.co>
2017-03-20 09:41:14 +0100

    Implement advanced login

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,14 +4,35 @@ require 'rails/test_help'
 require "minitest/reporters"
 Minitest::Reporters.use!
 
-
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-  include ApplicationHelper
-  # Add more helper methods to be used by all tests here...
+
+  # Returns true if a test user is logged in.
   def is_logged_in?
     !session[:user_id].nil?
   end
 
+  # Log in as a particular user.
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
+
+  def full_title(page_title = '')
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
+end
+
+class ActionDispatch::IntegrationTest
+
+  # Log in as a particular user.
+  def log_in_as(user, password: 'password', remember_me: '1')
+    post login_path, params: { session: { email: user.email,
+                                          password: password,
+                                          remember_me: remember_me } }
+  end
 end
