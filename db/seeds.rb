614765fb5ed61279ed14ef4db7d55a1424b3adb0
whynotfr <charles@offwefly.co>
2017-03-27 12:26:18 +0200

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -23,3 +23,11 @@ User.create!(name:  "Example User",
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
   end
+
+  # Following relationships
+users = User.all
+user  = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -46,6 +46,14 @@ test "should redirect edit when logged in as wrong user" do
     end
     assert_redirected_to root_url
   end
+    test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
 
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 
 end
